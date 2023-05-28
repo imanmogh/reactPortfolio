0003c33b6c8913c7ac74486e743c61a72a29c3b1
Iman Moghaddas <imanmoghaddas@yahoo.com>
2023-05-28 01:37:08 -0400

    Added my profile picture as well as Portfolio button page

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,8 +1,29 @@
 import React from 'react'
-import second from '../assets/profilePic.jpeg'
+import profilepic from '../assets/profilePic.jpeg'
+import {MdOutlineKeyboardArrowRight} from "react-icons/md"
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+            <div className='flex flex-col justify-center h-full'>
+                <h2 className='text-4xl sm::text-7xl font-bold text-white'>I am a Full Stack Developer</h2>
+                <p className='text-grey-500 py-4 max-w-md'>
+                    This is just a sample of the parragraph that i will be adding to this profile
+                </p>
+            </div>
+            <div>
+                <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                    Portfolio
+                    <span className='group-hover:rotate-90 duration-300'>
+                        <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+                    </span>
+                </button>
+            </div>
+            <div>
+                <img src={profilepic} alt='profile-picture' className='rounded-2xl mx-auto w-2/3 md:w-full '></img>
+            </div>
+        </div>
+    </div>
   )
 }
